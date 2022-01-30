0f316c6a48da42af6aa296519aa05c083682d886
shobanag2702@gmail.com <shobanag2702@gmail.com>
2022-01-30 16:13:10 +0530

    functional components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,75 +1,61 @@
-import React, {
-    Component
-} from 'react';
+import React from 'react';
 import { Card, CardImg,CardText, CardBody, CardTitle } from 'reactstrap';
 
-class Dishdetail extends Component {
-
-    componentDidMount()
+function RenderDish({dish})
+{
+    if(dish!=null)
     {
-      console.log('In dishdetail componentdidmount method')
+        return(
+            <div className="col-12 col-md-5 m-1">
+                <Card>
+                    <CardImg top src={dish.image} alt={dish.name} />
+                    <CardBody>
+                        <CardTitle>{dish.name}</CardTitle>
+                        <CardText>{dish.description}</CardText>
+                    </CardBody>
+                </Card>
+            </div>
+            
+        )
     }
-    componentDidUpdate()
+    else
+        return(
+            <div></div>
+        );
+    
+}
+function RenderComments({comments})
+{
+    if (comments != null && comments !== undefined)
     {
-      console.log('In dishdetail componentdidupdate method')
+        const commentList = comments.map(item => (
+            <li>
+            {item.comment}
+            <br/><br/>
+            -- {item.author},  {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(item.date)))}
+            <br/><br/>
+            </li>
+        )
+        );
+        return (
+            <div className="col-12 col-md-5 m-1">
+               <h4>Comments</h4> 
+               <ul className="list-unstyled">{commentList} </ul>
+            </div>
+        )
     }
-    renderDish(dish)
-    {
-        if(dish!=null)
-        {
-            return(
-                <div className="col-12 col-md-5 m-1">
-                    <Card>
-                        <CardImg top src={dish.image} alt={dish.name} />
-                        <CardBody>
-                            <CardTitle>{dish.name}</CardTitle>
-                            <CardText>{dish.description}</CardText>
-                        </CardBody>
-                    </Card>
-                </div>
-                
-            )
-        }
-        else
-            return(
-                <div></div>
-            );
-        
-    }
-    renderComments(comments)
-    {
-        if (comments != null && comments !== undefined)
-        {
-            const commentList = comments.map(item => (
-                <li>
-                {item.comment}
-                <br/><br/>
-                -- {item.author},  {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(item.date)))}
-                <br/><br/>
-                </li>
-            )
-            );
-            return (
-                <div className="col-12 col-md-5 m-1">
-                   <h4>Comments</h4> 
-                   <ul className="list-unstyled">{commentList} </ul>
-                </div>
-            )
-        }
-        else
-            return(
-                <div></div>
-            );
-    }
-    render()
-    {
-        console.log('In dishdetail render method')
-        if(this.props.dish != null && typeof this.props.dish!=="undefined")
+    else
+        return(
+            <div></div>
+        );
+}
+const  Dishdetail = (props) => {
+    if(props.dish != null && typeof props.dish!=="undefined")
         {
             return (
                 <div className="row">
-                    {this.renderDish(this.props.dish)}
-                    {this.renderComments(this.props.dish.comments)}
+                    <RenderDish dish={props.dish}></RenderDish>
+                    <RenderComments comments={props.dish.comments}></RenderComments>
                 </div>
             );
         }
@@ -77,7 +63,5 @@ class Dishdetail extends Component {
             return(
                 <div></div>
             );
-        
-    }
 }
 export default Dishdetail;
