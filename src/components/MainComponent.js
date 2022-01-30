875635a96e3e5d6e9e65dcf7449817b3c7ef684a
shobanag2702@gmail.com <shobanag2702@gmail.com>
2022-01-30 21:21:52 +0530

    assignment 2

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -7,7 +7,9 @@ import { COMMENTS } from '../shared/comments';
 import { PROMOTIONS } from '../shared/promotions';
 import { LEADERS } from '../shared/leaders';
 import Home from './HomeComponent';
+import Dishdetail from './DishdetailComponent';
 import Contact from './ContactComponent';
+import About from './AboutComponent';
 import { Redirect,Route, Switch } from 'react-router-dom';
 class Main extends Component {
   constructor(props) {
@@ -31,6 +33,12 @@ class Main extends Component {
     />
       );
     }
+    const DishWithId = ({match}) => {
+      return(
+          <Dishdetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
+            comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
+      );
+    };
     return (
       <div>
         <Header/>
@@ -38,6 +46,8 @@ class Main extends Component {
           <Route path='/home' component={HomePage} />
           <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
           <Route exact path='/contactus' component={Contact} />
+          <Route exact path='/aboutus' component={() => <About leaders={this.state.leaders} />}/>
+          <Route path='/menu/:dishId' component={DishWithId} />
           <Redirect to="/home" />
         </Switch>
         <Footer/>
