33fe46a89d5d8c9c47ccf61f6087632c27b09cb6
shobanag2702@gmail.com <shobanag2702@gmail.com>
2022-02-01 15:37:09 +0530

    assignment 3

--- src/components/CommentFormComponent.js
+++ src/components/CommentFormComponent.js
@@ -1,7 +1,12 @@
-import React, {Component} from 'react';
-import { Card, CardImg,CardText, CardBody, CardTitle,Breadcrumb, BreadcrumbItem,Button, Modal, ModalBody, ModalHeader, Label, Row, Col } from 'reactstrap';
-import { Link } from 'react-router-dom';
+import React, { Component } from 'react';
+
+import {   
+    Button, Modal, ModalBody, ModalHeader, Label, Row, Col
+} from "reactstrap";
+
 import { Control, LocalForm, Errors } from 'react-redux-form';
+
+//// validators
 const required = (val) => val && val.length; //value > 0
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
 const minLength = (len) => (val) => (val) && (val.length >= len);
@@ -150,84 +155,5 @@ class CommentForm extends Component{
         );
     }
 }
-function RenderDish({dish})
-{
-    if(dish!=null)
-    {
-        return(
-            <div className="col-12 col-md-5 m-1">
-                <Card>
-                    <CardImg top src={dish.image} alt={dish.name} />
-                    <CardBody>
-                        <CardTitle>{dish.name}</CardTitle>
-                        <CardText>{dish.description}</CardText>
-                    </CardBody>
-                </Card>
-            </div>
-            
-        )
-    }
-    else
-        return(
-            <div></div>
-        );
-    
-}
-function RenderComments({dish,comments})
-{
-    if (comments != null && comments !== undefined)
-    {
-        const commentList = comments.map(item => (
-            <li>
-            {item.comment}
-            <br/><br/>
-            -- {item.author},  {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(item.date)))}
-            <br/><br/>
-            </li>
-        )
-        );
-        return (
-            <div className="col-12 col-md-5 m-1">
-               <h4>Comments</h4> 
-               <ul className="list-unstyled">{commentList} </ul>
-               <CommentForm dish={dish} comments={comments} />
-            </div>
-        )
-    }
-    else
-        return(
-            <div></div>
-        );
-}
-const  Dishdetail = (props) => {
-    if(props.dish != null && typeof props.dish!=="undefined")
-        {
-            return (
-                <div className="container">
-                <div className="row">
-                    <Breadcrumb>
-                        <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
-                        <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
-                    </Breadcrumb>
-                    <div className="col-12">
-                        <h3>{props.dish.name}</h3>
-                        <hr />
-                    </div>                
-                </div>
-                <div className="row">
-                    <div className="col-12 col-md-5 m-1">
-                        <RenderDish dish={props.dish} />
-                    </div>
-                    <div className="col-12 col-md-5 m-1">
-                        <RenderComments dish={props.dish} comments={props.comments} />
-                    </div>
-                </div>
-                </div>
-            );       
-        }
-        else
-            return(
-                <div></div>
-            );
-}
-export default Dishdetail;
+
+export default CommentForm;
